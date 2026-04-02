fd6d7d16ab35bc3e4fc01e6bc37791aa83135988
shashanksharma-jpg <shashank.sharma@gmail.com>
2026-04-02 11:51:35 +0800

    Update middleware.js

--- middleware.js
+++ middleware.js
@@ -1,2 +1,4 @@
-export function middleware() {}
-export const config = { matcher: [] };
+export { default } from "next-auth/middleware";
+export const config = {
+  matcher: ["/((?!api/auth|auth/signin|_next/static|_next/image|favicon.ico).*)"],
+};
